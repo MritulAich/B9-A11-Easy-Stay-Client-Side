f1b83a4ad5e37d8962ba1ec6c764847c19b454ed
MritulAich <mridulaich332@gmail.com>
2024-06-28 20:40:24 +0600

    map added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
 import Banner from "../shared/homepage/Banner";
+import Map from "../shared/homepage/Location";
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
              <Banner></Banner>
+             <Map></Map>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
---
+++ src/shared/homepage/Location.jsx
@@ -0,0 +1,13 @@
+import { Map, Marker } from "pigeon-maps"
+
+const Location = () => {
+    return (
+        <div>
+            <Map height={300} defaultCenter={[29.117151854214754, 110.47877929663593]} defaultZoom={14}>
+                <Marker width={50} anchor={[29.117151854214754, 110.47877929663593]} />
+            </Map>
+        </div>
+    );
+};
+
+export default Location;
